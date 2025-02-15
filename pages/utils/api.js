606745d3c795c80c5115adbd34614f8d446fed75
brianedwin <brian.edwin28@gmail.com>
2025-02-15 23:30:51 +0000

    Delete pages/utils directory
    delete folder

--- pages/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-});
-
-export const registerUser = (data) => API.post("/auth/register", data);
-export const loginUser = (data) => API.post("/auth/login", data);
-export const getUser = (token) => API.get("/auth/user", { headers: { Authorization: `Bearer ${token}` } });
